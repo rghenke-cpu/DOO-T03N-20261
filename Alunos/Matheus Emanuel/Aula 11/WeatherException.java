d8592bdad024be4a7c8b668dfdcae8cbd2007f4c
yMatHezZz <matheuseoliveira44@gmail.com>
2026-06-12 19:41:27 -0300

    feat: aula 11

---
+++ Alunos/Matheus Emanuel/Aula 11/WeatherException.java
@@ -0,0 +1,10 @@
+public class WeatherException extends Exception {
+
+    public WeatherException(String message) {
+        super(message);
+    }
+
+    public WeatherException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
